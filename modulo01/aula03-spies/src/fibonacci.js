c5bf59e36d418bf678fec8d95f91cde66d9a14e8
Larissa Pissurno <larissapissurno@gmail.com>
2022-02-15 22:05:51 -0300

    Reflect

--- modulo01/aula03-spies/src/fibonacci.js
+++ modulo01/aula03-spies/src/fibonacci.js
@@ -1,12 +1,13 @@
 class Fibonacci {
   *execute(input, current = 0, next = 1) {
-    if(input === 0) {
+    if (input === 0) {
       return 0;
     }
+    // returns the value
+    yield current;
 
-    yield current
-
-    yield* this.execute(input-1, next, current + next)
+    // delegate the function but doesn't returns the value
+    yield* this.execute(input - 1, next, current + next);
   }
 }
 
